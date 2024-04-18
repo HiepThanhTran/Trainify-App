9f086199f3e480721ce4a29f1843436e09670d45
HiepThanhTran <hieptt.2003@gmail.com>
2024-04-18 13:44:34 +0700

    config settings

--- TrainingPointManagement/settings.py
+++ TrainingPointManagement/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-6*^0g#i2%(w@-d(!f$aylqv89yht0!_4f$=$%0%d#fk8%uh6*g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'tpm-api.pythonanywhere.com',
+]
 
 
 # Application definition
