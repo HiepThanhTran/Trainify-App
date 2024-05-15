910031505c56c1b1286cad6b6f547dabc5eb59b6
HiepThanhTran <hieptt.2003@gmail.com>
2024-05-15 17:57:31 +0700

    remove cached

--- build_files.sh
+++
@@ -1,5 +0,0 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate
-python3.9 manage.py collectdata
--- interacts/admin.py
+++
@@ -1,6 +0,0 @@
-# from django.contrib import admin
-from core.admin import my_admin_site
-from interacts.models import Comment, Like
-
-my_admin_site.register(Comment)
-my_admin_site.register(Like)
