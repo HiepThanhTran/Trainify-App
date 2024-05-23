6b613438994819bd67b42db0f18ddba641a1a6a0
HiepThanhTran <hieptt.2003@gmail.com>
2024-05-23 22:26:01 +0700

    change database to postgres

--- core/settings.py
+++ core/settings.py
@@ -98,20 +98,20 @@ pymysql.install_as_MySQLdb()
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-	# "default": {
-	# 	"ENGINE": "django.db.backends.postgresql_psycopg2",
-	# 	"NAME": os.getenv("POSTGRES_DATABASE"),
-	# 	"USER": os.getenv("POSTGRES_USER"),
-	# 	"PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-	# 	"HOST": os.getenv("POSTGRES_HOST"),
-	# },
 	"default": {
-		"ENGINE": "django.db.backends.mysql",
-		"NAME": "training_point_db",
-		"USER": "root",
-		"PASSWORD": "29012003",
-		"HOST": "localhost",
+		"ENGINE": "django.db.backends.postgresql_psycopg2",
+		"NAME": os.getenv("POSTGRES_DATABASE"),
+		"USER": os.getenv("POSTGRES_USER"),
+		"PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+		"HOST": os.getenv("POSTGRES_HOST"),
 	},
+	# "default": {
+	# 	"ENGINE": "django.db.backends.mysql",
+	# 	"NAME": "training_point_db",
+	# 	"USER": "root",
+	# 	"PASSWORD": "29012003",
+	# 	"HOST": "localhost",
+	# },
 	# "default": {
 	#     "ENGINE": "django.db.backends.sqlite3",
 	#     "NAME": BASE_DIR / "db.sqlite3",
