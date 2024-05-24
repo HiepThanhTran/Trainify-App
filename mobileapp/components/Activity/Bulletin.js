6f126a985e4de3899cae94c9d2b5655f243fea1b
SongHau <songhau2772003@gmail,com>
2024-05-24 20:50:13 +0700

    fix bulletin

--- mobileapp/components/Activity/Bulletin.js
+++ mobileapp/components/Activity/Bulletin.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, TextInput, Image, TouchableOpacity, ActivityIndicator, ScrollView, RefreshControl } from "react-native";
+import { View, Text, TextInput, Image, TouchableOpacity, ActivityIndicator, ScrollView, RefreshControl, Keyboard } from "react-native"; 
 import { AntDesign } from '@expo/vector-icons';
-import { useNavigation } from "@react-navigation/native";
 import { Dimensions } from "react-native";
 import RenderHTML from "react-native-render-html";
 import GlobalStyle from '../../styles/Style';
@@ -16,22 +15,27 @@ const screenWidth = Dimensions.get('window').width;
 const Bulletin = ({ navigation }) => {
     const [bulletins, setBulletins] = useState([]);
     const [loading, setLoading] = useState(false);
-    const [page, setPage] = useState(1)
+    const [page, setPage] = useState(1);
+    const [q, setQ] = useState("");
+    const [canLoadMore, setCanLoadMore] = useState(true);
+
     const loadBulletins = async () => {
         if (page > 0) {
-            let url = `${endPoints['bulletins']}?page=${page}`;
+            setLoading(true);
             try {
-                setLoading(true);
-                let res = await APIs.get(url);
-                if(page===1){
-                    setBulletins(res.data.results);
-                }else if(page>1){
-                    setBulletins(current => {
-                        return [...current, ...res.data.results]
-                    })
+                let url = `${endPoints['bulletins']}?page=${page}`;
+                if (q) {
+                    url += `&title=${encodeURIComponent(q)}`;
                 }
+                let res = await APIs.get(url);
                 if (res.data.next === null) {
                     setPage(0);
+                    setCanLoadMore(false);
+                }
+                if (page === 1) {
+                    setBulletins(res.data.results);
+                } else {
+                    setBulletins(current => [...current, ...res.data.results]);
                 }
             } catch (ex) {
                 console.error(ex);
@@ -43,20 +47,29 @@ const Bulletin = ({ navigation }) => {
 
     useEffect(() => {
         loadBulletins();
-    }, [page]);
+    }, [page, q]);
 
     const loadMore = ({ nativeEvent }) => {
-        if (!loading && isCloseToBottom(nativeEvent)) {
+        if (!loading && canLoadMore && isCloseToBottom(nativeEvent)) {
             setPage(page + 1);
         }
+    };
+
+    const search = (value) => {
+        setPage(1);
+        setQ(value);
     };
 
     const go = (bulletinID) => {
         navigation.navigate('BulletinDetail', { 'bulletinID': bulletinID })
     };
 
+    const dismissKeyboard = () => {
+        Keyboard.dismiss();
+    };
+
     return (
-        <View style={GlobalStyle.BackGround}>
+        <View style={GlobalStyle.BackGround} onTouchStart={dismissKeyboard}>
             <View style={BulletinStyle.Container}>
                 <View style={BulletinStyle.TopContainer}>
                     <Text style={BulletinStyle.Text}>Bản tin</Text>
@@ -66,14 +79,19 @@ const Bulletin = ({ navigation }) => {
                     <TextInput
                         style={BulletinStyle.SearchInput}
                         placeholder="Tìm kiếm bản tin"
+                        onChangeText={search}
+                        value={q}
                     />
                 </View>
 
-                <ScrollView style={BulletinStyle.BulletinCardContainer} onScroll={loadMore}>
-                    <RefreshControl onRefresh={() => loadBulletins()} />
+                <ScrollView style={BulletinStyle.BulletinCardContainer} onScroll={loadMore} 
+                     showsVerticalScrollIndicator={false}
+                     showsHorizontalScrollIndicator={false}
+                >
+                    <RefreshControl onRefresh={loadBulletins} />
                     {loading && <ActivityIndicator />}
-                    {bulletins.map((bulletin, index) => (
-                        <TouchableOpacity key={index} onPress={() => go(bulletin.id)}>
+                    {bulletins.map((bulletin) => (
+                        <TouchableOpacity key={bulletin.id} onPress={() => go(bulletin.id)}>
                             <View style={BulletinStyle.BulletinCard}>
                                 <View style={BulletinStyle.BulletinCardImage}>
                                     <Image
@@ -95,7 +113,7 @@ const Bulletin = ({ navigation }) => {
                                         }}
                                     />
 
-                                    <Text style={BulletinStyle.Date}>Ngày tạo: {formatDate(bulletin.created_date)}</Text>
+                                    <Text style={BulletinStyle.Date}>Ngày tạo: <Text>{formatDate(bulletin.created_date)}</Text></Text>
                                 </View>
                             </View>
                         </TouchableOpacity>
