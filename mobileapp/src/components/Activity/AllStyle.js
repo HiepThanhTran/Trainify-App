c854e77c8e1faf0301a88a67772bb53165042368
SongHau <songhau2772003@gmail,com>
2024-05-29 17:49:31 +0700

    pull

--- mobileapp/src/components/Activity/AllStyle.js
+++ mobileapp/src/components/Activity/AllStyle.js
@@ -131,7 +131,7 @@ export default AllStyle = StyleSheet.create({
     },
     More: {
         fontFamily: Theme.Bold,
-        fontSize: 18,
+        fontSize: 17.2,
         marginTop: -15,
         marginBottom: 10
     },
@@ -167,4 +167,16 @@ export default AllStyle = StyleSheet.create({
         marginLeft: 10,
         padding: 5,
     },
+    ReactContainer:{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 20
+    },
+    Like:{
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    }
 });
