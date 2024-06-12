0c25f72063a6349aa1a3442295b5138a0da5011f
SongHau <songhau2772003@gmail,com>
2024-06-13 02:56:11 +0700

    fix edit activity

--- src/components/profile/All.js
+++ src/components/profile/All.js
@@ -2,13 +2,36 @@ import { StyleSheet } from "react-native";
 import Theme from "../../styles/Theme";
 
 export default All = StyleSheet.create({
-    Container:{
-        marginTop: 20,
+    Container: {
+        marginTop: 30,
         marginRight: 16,
         marginLeft: 16,
     },
-    FormCreateActivity:{
+    FormCreateActivity: {
         
     },
-    
+    TextInputTitle:{
+        fontSize: 16,
+        fontFamily: Theme.SemiBold,
+        marginBottom: 10,
+    },
+    TextInputContainer: {
+        marginBottom: 20,
+    },
+    TextInputWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: 'blue',
+        borderRadius: 5,
+        paddingHorizontal: 10,
+    },
+    TextInput: {
+        flex: 1,
+        color: 'gray',
+        padding: 10,
+    },
+    Icon: {
+        marginLeft: 10,
+    },
 });
--- src/components/profile/CreateActivityForm.js
+++ src/components/profile/CreateActivityForm.js
@@ -1,23 +1,113 @@
-import { View, Text, TextInput } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, TextInput, ScrollView } from "react-native";
 import GlobalStyle from "../../styles/Style";
-import { Entypo } from '@expo/vector-icons';
+import { Entypo, Ionicons, Fontisto, FontAwesome, FontAwesome5, AntDesign } from '@expo/vector-icons';
 import All from "./All";
+import { DateTimePicker} from "@react-native-community/datetimepicker";
 
 const CreateActivityForm = () => {
-   return(
+   const [date, setDate] = useState(new Date());
+   const [showPicker, setShowPicker] = useState(false);
+
+   const handleDateChange = (event, selectedDate) => {
+      const currentDate = selectedDate || date;
+      setShowPicker(false);
+      setDate(currentDate);
+   };
+
+   const showDatePicker = () => {
+      setShowPicker(true);
+   };
+
+   return (
       <View style={GlobalStyle.BackGround}>
          <View style={All.Container}>
-            <View style={All.FormCreateActivity}>
+            <ScrollView style={All.FormCreateActivity}>
                <View style={All.TextInputContainer}>
-                  <Text>Tên hoạt động</Text>
-                  <View style={All.TextInput}>
-                     <Entypo name="news" size={24} color="black" />
+                  <Text style={All.TextInputTitle}>Hoạt động</Text>
+                  <View style={All.TextInputWrapper}>
                      <TextInput
                         placeholder="Tên hoạt động"
+                        style={All.TextInput}
                      />
+                     <Entypo name="news" size={24} color="black" style={All.Icon} />
                   </View>
                </View>
-            </View>
+
+               <View style={All.TextInputContainer}>
+                  <Text style={All.TextInputTitle}>Đối tượng</Text>
+                  <View style={All.TextInputWrapper}>
+                     <TextInput
+                        placeholder="Đối tượng tham gia"
+                        style={All.TextInput}
+                     />
+                     <Ionicons name="people" size={24} color="black" style={All.Icon} />
+                  </View>
+               </View>
+
+               <View style={All.TextInputContainer}>
+                  <Text style={All.TextInputTitle}>Ngày bắt đầu</Text>
+                  <TouchableOpacity onPress={showDatePicker} style={All.TextInputWrapper}>
+                     <Text style={All.TextInput}>{date.toDateString()}</Text>
+                     <AntDesign name="clockcircle" size={24} color="black" style={All.Icon}/>
+                  </TouchableOpacity>
+               </View>
+
+               <View style={All.TextInputContainer}>
+                  <Text style={All.TextInputTitle}>Địa điểm</Text>
+                  <View style={All.TextInputWrapper}>
+                     <TextInput
+                        placeholder="Tên địa điểm"
+                        style={All.TextInput}
+                     />
+                     <Ionicons name="location" size={24} color="black" style={All.Icon} />
+                  </View>
+               </View>
+
+               <View style={All.TextInputContainer}>
+                  <Text style={All.TextInputTitle}>Điểm cộng</Text>
+                  <View style={All.TextInputWrapper}>
+                     <TextInput
+                        placeholder="Điểm cộng cho sinh viên"
+                        style={All.TextInput}
+                     />
+                    <Fontisto name="star" size={24} color="black"  style={All.Icon}/>
+                  </View>
+               </View>
+
+               <View style={All.TextInputContainer}>
+                  <Text style={All.TextInputTitle}>Bảng tin</Text>
+                  <View style={All.TextInputWrapper}>
+                     <TextInput
+                        placeholder="Tên bảng tin"
+                        style={All.TextInput}
+                     />
+                    <FontAwesome5 name="newspaper" size={24} color="black"  style={All.Icon}/>
+                  </View>
+               </View>
+
+               <View style={All.TextInputContainer}>
+                  <Text style={All.TextInputTitle}>Khoa</Text>
+                  <View style={All.TextInputWrapper}>
+                     <TextInput
+                        placeholder="Tên khoa"
+                        style={All.TextInput}
+                     />
+                     <FontAwesome name="graduation-cap" size={24} color="black" style={All.Icon}/>
+                  </View>
+               </View>
+
+               {showPicker && (
+                  <DateTimePickerAndroid
+                     testID="dateTimePicker"
+                     value={date}
+                     mode="date"
+                     is24Hour={true}
+                     display="default"
+                     onChange={handleDateChange}
+                  />
+               )}
+            </ScrollView>
          </View>
       </View>
    )
